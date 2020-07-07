9e2927e28e6bc59704cc327af7c3b8f9cfe786be
kathimohan <36533398+kathimohan@users.noreply.github.com>
2020-07-07 10:39:59 +0530

    Update k.py

--- k.py
+++ k.py
@@ -1,15 +1,11 @@
-from keras.preprocessing.image import img_to_array
-from keras.models import load_model
-from keras.utils import get_file
-import matplotlib.pyplot as plt 
+
 import numpy as np
 import argparse
 import cv2
 import os
 import cvlib as cv
 import time
-import xlwt
-from xlwt import Workbook
+
 
 def ca(dir):
     for path,subdirnames,filenames in os.walk(dir, topdown=False):
